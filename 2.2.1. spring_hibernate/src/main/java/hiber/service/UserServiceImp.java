703198a20abbc2018f085941378be5e093bd2435
Vadim <vadim23452@yandex.ru>
2023-11-28 15:10:54 +0300

    по аналогии верхнего метода запустил свой метод

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -26,4 +26,12 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional(readOnly = true)
+   @Override
+   public List<User> getUserByModelAndSeries(String model, int series) {
+      return userDao.getUserByModelAndSeries(model,series);
+   }
+
+
+
 }
